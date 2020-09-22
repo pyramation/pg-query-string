8b810698aadd0aaf0632fdc6a45239b3360ed45b
Dan Lynch <pyramation@gmail.com>
2020-09-21 20:59:11 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# sql-query
+
+Thanks to https://github.com/hiddentao/squel
+
+This is essentially squel, however a few changes:
+
+* built with babel instead of gulp
+* hard-coded to postgres 
+* added type coersions 
---
+++ __tests__/index.test.js
@@ -0,0 +1,81 @@
+import squel from '../src';
+import cases from 'jest-in-case';
+
+it('insert', () => {
+  expect(
+    squel
+      .insert()
+      .into('users')
+      .set('username', 'pyramation', 'type1')
+      .set('email', 'pyramation@gmail.com', 'type2')
+      .returning('*')
+      .toParam({ casts: { username: 'int' } })
+  ).toMatchSnapshot();
+});
+
+it('update', () => {
+  expect(
+    squel
+      .update()
+      .table('students')
+      .set('name', 'Fred', 'text')
+      .set('age', 29)
+      .set('data', { a: 1, b: 2 }, 'jsonb')
+      .set('score', 1.2, 'numeric')
+      .set('graduate', false, 'boolean')
+      .set(
+        'level',
+        squel
+          .select()
+          .field('MAX(level)')
+          .from('levels')
+      )
+      .toParam()
+  ).toMatchSnapshot();
+});
+
+it('select', () => {
+  expect(
+    squel
+      .select()
+      .field('id')
+      .from('students')
+      .where(
+        squel
+          .expr()
+          .and("name = 'Thomas'")
+          .or('age > 18')
+      )
+      .toParam()
+  ).toMatchSnapshot();
+});
+
+it('where', () => {
+  expect(
+    squel
+      .select()
+      .field('id')
+      .from('students')
+      .where(
+        squel
+          .expr()
+          .and('name = ?', 'dude')
+          .or('age > ?', 18)
+      )
+      .limit(2)
+      .toParam()
+  ).toMatchSnapshot();
+});
+
+it('delete', () => {
+  expect(
+    squel
+      .delete()
+      .from('students')
+      .where('id > ?', 1)
+      .where('id < ?', 100)
+      .order('id', false)
+      .limit(5)
+      .toParam()
+  ).toMatchSnapshot();
+});
